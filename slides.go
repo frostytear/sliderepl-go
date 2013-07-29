52edc199784630d24963c6d233ee017f836688b2
Christopher Swenson <chris@caswenson.com>
2013-07-28 18:22:06 -0700

    Added sample slides

---
+++ slides.go
@@ -0,0 +1,16 @@
+//!
+// This is the basic hello, world program.
+import "fmt"
+
+fmt.Println("hello, world")
+
+//!
+// A simple for-loop
+import "fmt"
+
+for i := 0; i < 10; i++ {
+	fmt.Printf("i = %v\n", i)
+}
+//!
+// This is just a comment slide
+
